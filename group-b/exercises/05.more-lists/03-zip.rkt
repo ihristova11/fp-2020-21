4084feda05f186461134d22cc204c68ded77dc17
Irina Hristova <hristova119@gmail.com>
2020-11-28 20:04:31 +0200

    Update 03-zip.rkt

--- group-b/exercises/05.more-lists/03-zip.rkt
+++ group-b/exercises/05.more-lists/03-zip.rkt
@@ -6,9 +6,14 @@
 ; може да видим какво се очаква да прави в тестовете
 
 (define (zip xs ys)
-  (void)
-)
-
+  (define (helper xs ys res)
+    (if (or (null? xs) (null? ys))
+        res
+        (helper (cdr xs) (cdr ys) (append res (cons (cons (car xs) (list (car ys))) '())))
+    )
+    )
+  (helper xs ys '())
+  )
 (define tests
   (test-suite "Zip"
     (check-equal? (zip '(1 2 3) '(4 5 6)) '((1 4) (2 5) (3 6)))
