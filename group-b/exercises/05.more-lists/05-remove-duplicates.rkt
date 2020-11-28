b84ca88bff0b7a49c5e867f0d4c9b39a54cb5a56
Irina Hristova <hristova119@gmail.com>
2020-11-28 20:55:30 +0200

    Update 05-remove-duplicates.rkt

--- group-b/exercises/05.more-lists/05-remove-duplicates.rkt
+++ group-b/exercises/05.more-lists/05-remove-duplicates.rkt
@@ -2,11 +2,28 @@
 (require rackunit)
 (require rackunit/text-ui)
 
+(define (duplicate? x lst)
+  (define (helper x lst count)
+    (cond ((null? lst) #f)
+      ((> count 1) #t)
+      ((= (car lst) x) (helper x (cdr lst) (+ 1 count)))
+      (else (helper x (cdr lst) count))
+    )
+  )
+
+  (helper x lst 0) 
+)
+
 ; remove-duplicates 
 ; премахва всички повтарящи се елементи от списъка
-
 (define (remove-duplicates xs)
-  (void)
+  (define (helper xs res)
+  (cond ((null? xs) res)
+        ((not (duplicate? (car xs) res)) (helper (cdr xs) (cons (car xs) res)))
+        (else (helper (cdr xs) res))
+  )
+)
+  (helper xs '()) 
 )
 
 (define tests
