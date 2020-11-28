0511748797b9b2ad0f6a4b160f5f4208cdd1cea9
Irina Hristova <hristova119@gmail.com>
2020-11-28 21:22:45 +0200

    Update 06-chunk.rkt

--- group-b/exercises/05.more-lists/06-chunk.rkt
+++ group-b/exercises/05.more-lists/06-chunk.rkt
@@ -6,9 +6,19 @@
 ; разбива списъка xs на подсписъци с дължина n
 
 (define (chunk n xs)
-  (void)
-)
+  (define (safe-take n xs)
+    (cond ((> n (length xs)) xs)
+          (else (take xs n)))
+  )
 
+  (define (safe-drop n xs)
+    (cond ((> n (length xs)) '())
+          (else (drop xs n))))
+  
+  (if (null? xs)
+      '()
+      (cons (safe-take n xs) (chunk n (safe-drop n xs))))
+)
 (define tests
   (test-suite "chunk"
     (check-equal? (chunk 2 '(1 2 3 4 5 6 7 8 9))  '((1 2) (3 4) (5 6) (7 8) (9)))
